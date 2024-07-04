8a6e6c71cba69d7544ae69ff7ce81d848654898c
SUN PENGFEI <87409761+AnonymouslyNetting@users.noreply.github.com>
2024-07-04 18:32:25 +0900

    Update Rejsam_y.m

--- Rejsam_y.m
+++ Rejsam_y.m
@@ -4,7 +4,7 @@ function    rej_y_res = Rejsam_y(rho_apostrophe, row, security_level)
     poly_cnt = 0;
     for i = 1:2000
         if(poly_cnt < 256)
-            if(security_level == 2)%security_level3时rejsam_y规则略有不同
+            if(security_level == 2)
                 rej_y_res(poly_cnt+1) = bin2dec(flip(reshape(char(res((18*i-17):(18*i))+'0'),18,''))');
                 poly_cnt = poly_cnt + 1;
             else
